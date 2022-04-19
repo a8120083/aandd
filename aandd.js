38869ac840247446c14fcf69de432a9ba804906c
近藤亮太 <a8120083@gsuite.si.aoyama.ac.jp>
2022-04-19 18:25:34 +0900

    練習問題1-1から1-4終わりました。

---
+++ aandd.js
@@ -0,0 +1,18 @@
+function show(array) {
+    var html = '';
+    html = html + '<table border="1">';
+    html = html + '<tr>';
+    for (var i = 0; i < array.length; i++) {
+        html = html + '<td>' + array[i] + '</td>';
+    }
+    html = html + '</tr>';
+    html = html + '</table>';
+    //document.getElementById('res').innerHTML = html;
+    document.getElementById('res').insertAdjacentHTML('beforeend', html);
+}
+
+function createSequentialNumbers(array, n) {
+    for (var i = 1; i <= n; i++){
+        array.push(i);
+    }
+}
