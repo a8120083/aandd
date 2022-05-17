7558cd8bebd19378b6e5e7635c2f420c1b8faa78
近藤亮太 <a8120083@gsuite.si.aoyama.ac.jp>
2022-05-17 20:01:00 +0900

    dekimasita

--- aandd.js
+++ aandd.js
@@ -1,14 +1,20 @@
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
-    html = html + '</tr>';
-    html = html + '</table>';
-    //document.getElementById('res').innerHTML = html;
-    document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
 
 function createSequentialNumbers(array, n) {
@@ -28,5 +34,18 @@ function createRandomNumbersNew(array, n){
     for (var i = 1; i <= n; i++){
         var num = Math.floor(Math.random()*20) + 1;
         array.push(num);
+    }
+}
+
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
     }
 }
